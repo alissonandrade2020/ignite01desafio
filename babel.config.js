5156dc118893aaa4f1e78a0eb37987f6a68e178b
Alisson de Andrade Araújo <alisson2d@hotmail.com>
2021-03-09 18:09:37 -0300

    Desefaio 01 - Ignite!

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,8 @@ module.exports = {
     ['@babel/preset-react', {
       runtime: 'automatic'
     }]
-  ]
+  ],
+  plugins: [
+    "inline-import-data-uri"
+  ],
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import '../styles/header.scss'
 
+import logo from '../../public/logo.svg';
+
 export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="../logo.svg" alt="to.do"/>
+        <img src={logo} alt="to.do"/>
       </div>
     </header>
   )
