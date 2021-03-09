fc9903a8bb993ccc60f0290d1608d5b7c8dacbca
Alisson de Andrade Araújo <alisson2d@hotmail.com>
2021-03-09 16:31:22 -0300

    Desefaio 01 - Ignite

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        <img src="../logo.svg" alt="to.do"/>
       </div>
     </header>
   )
